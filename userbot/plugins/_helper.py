444fec230d733b333d220560f4f22bba08c0367a
HUNTER <73104132+HUNTER2213@users.noreply.github.com>
2020-10-19 20:21:49 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -73,7 +73,7 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""Telethon UserBot Powered by @HellBot_Official""")
+    await event.edit("""Telethon UserBot Powered by @HUNTER_OFFICAL_BOT""")
 
 
 @borg.on(admin_cmd(pattern="syntax (.*)"))
